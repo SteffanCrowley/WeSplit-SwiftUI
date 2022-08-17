fcf81657f696211d760028ef2d178f4e416b4c31
Steffan Crowley <steffancrow@gmail.com>
2022-08-17 14:01:49 -0700

    add button tap count

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
