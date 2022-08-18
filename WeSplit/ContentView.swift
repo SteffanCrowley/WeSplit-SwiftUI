d2d1446aabacd79295a26401695192a97f33db8e
Steffan Crowley <steffancrow@gmail.com>
2022-08-18 08:35:59 -0700

    add #of people picker

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,19 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     let students = ["harry","ron", "hermione"]
-    @State private var selectedStudent = "harry"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        NavigationView {
-            
+        NavigationView{
             Form {
-                Picker("select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code:"USD"))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach (0..<100) {
+                            Text("\($0) people")
+                        }
                     }
+                      }
+                Section {
+                    Text(checkAmount, format: .currency(code:"USD"))
                 }
+                
                 }
-            }
+            .navigationTitle("WeSplit")
+        }
+
         }
 
     }
